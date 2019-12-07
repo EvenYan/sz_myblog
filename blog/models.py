e94e157665f63f748f52365bc5d8720809a4b357
even <1014934663@qq.com>
2019-12-07 16:21:09 +0800

    模型关系，类管理器

--- blog/models.py
+++ blog/models.py
@@ -2,12 +2,35 @@ from django.db import models
 
 # Create your models here.
 
+class MyManager(models.Manager):
+    def count_title(self, kw):
+        return self.filter(title__contains=kw).count()
+
+
+class NormalManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().exclude(excerpt__exact='Secret')
+
+
+class SecretManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(excerpt__exact='Secret')
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField(blank=False)
     views = models.PositiveIntegerField(default=100, null=True)
     created_time = models.DateTimeField(auto_now_add=True, null=True)
+    tag = models.ManyToManyField(Tag)
+    excerpt = models.CharField(max_length=100, default="Normal")
+    my_objects = MyManager()
+    normal_objects = NormalManager()
+    secret_objects = SecretManager()
 
     def __str__(self):
         return self.title
@@ -40,3 +63,12 @@ class Student(models.Model):
     name = models.CharField(max_length=100)
     sex = models.BooleanField(default=True)
     grade = models.ForeignKey(Grade)
+
+
+class People(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class IDCard(models.Model):
+    num = models.CharField(max_length=18)
+    people = models.OneToOneField(People, on_delete=models.PROTECT)
